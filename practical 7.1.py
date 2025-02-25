2750283eaac30cbb6584ddd1babd16aa5ecee46e
swayam34ai <dswayam33@gmail.com>
2025-02-25 15:28:34 +0530

    Add files via upload

---
+++ practical 7.1.py
@@ -0,0 +1,5 @@
+d1={1:"swayam",2:"yash"}
+d2={3:"dev",4:"preet"}
+d3={5:"daivik",6:"atharva"}
+combined={**d1,**d2,**d3}
+print(combined)
---
+++ practical 7.2.py
@@ -0,0 +1,6 @@
+d={}
+a=len(d)
+if  a==0:
+    print("empty dictionary")
+else:
+    print("not empty")
